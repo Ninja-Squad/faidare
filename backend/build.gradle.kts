f60b0a998de25d6dbb69eb77407d5ace04e1311e
jnizet <jb@ninja-squad.com>
2023-11-24 17:24:34 +0100

    feat: enable swagger

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -109,8 +109,7 @@ dependencies {
 
     implementation("org.springframework.cloud:spring-cloud-starter-config")
 
-    // Swagger
-    implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
     // Others
     implementation("com.google.guava:guava:33.4.0-jre")
