ed060b94c25fadd53e24e34a4124cf5ee6fbc445
jnizet <jb@ninja-squad.com>
2024-01-05 10:33:27 +0100

    feat: add Collection Call workaround.
    Need to implement a full index later

--- backend/src/main/java/fr/inrae/urgi/faidare/domain/CollPopVO.java
+++ backend/src/main/java/fr/inrae/urgi/faidare/domain/CollPopVO.java
@@ -1,16 +1,28 @@
 package fr.inrae.urgi.faidare.domain;
 
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(
+    indexName = "faidare_germplasm_dev-group0",
+    //indexName = "faidare_germplasm_beta-group0",
+    createIndex = false
+)
 public class CollPopVO {
 
     private Integer germplasmCount;
 
     private PuiNameValueVO germplasmRef;
 
-    private Long id;
+    private String id;
 
     private String name;
 
     private String type;
+
+    public CollPopVO(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 
 
     public Integer getGermplasmCount() {
@@ -29,11 +41,11 @@ public class CollPopVO {
         this.germplasmRef = germplasmRef;
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
