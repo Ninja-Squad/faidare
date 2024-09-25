dcf08c73973c2e2deb03bf0f7c6c8e11077f7ca6
Najwa Lakmouri <najwa.lakmouri@inrae.fr>
2024-09-25 18:29:40 +0200

    set faidareProperties as @Primary to resolve bean injection conflict

--- backend/src/main/java/fr/inrae/urgi/faidare/config/ElasticSearchConfig.java
+++ backend/src/main/java/fr/inrae/urgi/faidare/config/ElasticSearchConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ElasticSearchConfig {
@@ -56,6 +57,7 @@ public class ElasticSearchConfig {
      * "faidarePropertiesBean"so that "faidarePropertiesBean" can be used
      * in SpEL expressions
      */
+    @Primary
     @Bean
     public FaidareProperties faidarePropertiesBean() {
         return this.faidareProperties;
