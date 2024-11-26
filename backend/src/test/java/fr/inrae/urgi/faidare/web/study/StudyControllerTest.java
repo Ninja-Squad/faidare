546bde6baa215630c5e5fdd6c0230f85957bafa3
LAKMOURI NAJWA <najwa.lakmouri@inrae.fr>
2024-11-26 18:35:24 +0100

    Update StudyControllerTest.java

--- backend/src/test/java/fr/inrae/urgi/faidare/web/study/StudyControllerTest.java
+++ backend/src/test/java/fr/inrae/urgi/faidare/web/study/StudyControllerTest.java
@@ -121,7 +121,6 @@ public class StudyControllerTest {
                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                .andExpect(htmlContent().hasTitle("Study Doability: Study 1"))
                .andExpect(htmlContent().containsH2s("Identification", "Genotype", "Variables", "Data Set", "Contact", "Additional information", "Cross references"))
-                .andExpect(htmlContent().contains("http://example.com"))
                 .andExpect(htmlContent().endsCorrectly());
     }
 
